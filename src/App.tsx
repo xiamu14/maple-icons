612d55fce067b95e46848e785affa8c052e446db
ben <x_z_gfree@163.com>
2022-04-26 23:54:11 +0800

    修改 完成基本的展示和复制功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,118 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useCallback, useMemo } from 'react'
+import { useToast } from '@chakra-ui/react'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const toast = useToast()
+  const icons = useMemo(() => {
+    const modules = import.meta.globEager('./package/*.tsx')
+    const iconArray = Object.keys(modules).map((key) => {
+      const nameGroup = key.split('/')
+      const iconName = nameGroup[nameGroup.length - 1].replace('.tsx', '')
+      return { iconName, component: modules[key]?.default }
+    })
+    return iconArray
+  }, [])
+
+  const filledIcons = useMemo(() => {
+    return icons.filter((it) => it.iconName.includes('filled'))
+  }, [])
+
+  const lightIcons = useMemo(() => {
+    return icons.filter((it) => it.iconName.includes('light'))
+  }, [])
+
+  const otherIcons = useMemo(() => {
+    return icons.filter(
+      (it) => !it.iconName.includes('light') && !it.iconName.includes('filled')
+    )
+  }, [])
+
+  const handleCopy = useCallback(async (iconName) => {
+    const code = `<${toHump(iconName)} />`
+    try {
+      await navigator.clipboard.writeText(code)
+      toast({
+        title: code,
+        description: '已复制到剪贴板',
+        status: 'success',
+        position: 'top',
+        duration: 3000,
+        isClosable: true,
+      })
+    } catch (error) {}
+  }, [])
 
   return (
+    // <React.Suspense fallback="">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="search-bar" />
+      <div className="icon-panel">
+        {filledIcons.map((item) => {
+          const Component = item.component
+          return (
+            <div
+              className="icon-box"
+              key={item.iconName}
+              onClick={() => handleCopy(item.iconName)}
+            >
+              <div className="icon-block">
+                <Component width={30} height={30} />
+              </div>
+              <p className="icon-name">{item.iconName}</p>
+            </div>
+          )
+        })}
+      </div>
+      <div className="icon-panel">
+        {lightIcons.map((item) => {
+          const Component = item.component
+          return (
+            <div
+              className="icon-box"
+              key={item.iconName}
+              onClick={() => handleCopy(item.iconName)}
+            >
+              <div className="icon-block">
+                <Component width={30} height={30} />
+              </div>
+              <p className="icon-name">{item.iconName}</p>
+            </div>
+          )
+        })}
+      </div>
+      <div className="icon-panel">
+        {otherIcons.map((item) => {
+          const Component = item.component
+          return (
+            <div
+              className="icon-box"
+              key={item.iconName}
+              onClick={() => handleCopy(item.iconName)}
+            >
+              <div className="icon-block">
+                <Component width={30} height={30} />
+              </div>
+              <p className="icon-name">{item.iconName}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
+    // </React.Suspense>
   )
 }
 
 export default App
+
+// 下划线转驼峰
+function toHump(name: string) {
+  return name
+    .split('_')
+    .map((it) => {
+      return it.replace(/^\w/g, (it) => {
+        return it.toUpperCase()
+      })
+    })
+    .join('')
+}
